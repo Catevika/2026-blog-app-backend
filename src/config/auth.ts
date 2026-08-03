153d8499f3286bf7f308a1cc2c6eb8785dfc3245
Catevika <dominique.bello@outlook.fr>
2026-08-03 21:19:33 +0200

    Back to previous cookie options // Webkit issue with http cookies

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -16,7 +16,7 @@ export const AUTH = {
 
 	COOKIE_OPTIONS: {
 		httpOnly: true,
-		secure: process.env?.["NODE_ENV"] === "production" ? true : false,
-		sameSite: process.env?.["NODE_ENV"] === "production" ? "none" : "strict",
+		secure: process.env?.["NODE_ENV"] === "production",
+		sameSite: process.env?.["NODE_ENV"] === "production" ? "none" : "lax",
 	},
 } as const;
